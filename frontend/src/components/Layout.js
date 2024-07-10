ede5522f273cac2c6876cf40808dabb904689b01
krishn113 <ag.krishna0807@gmail.com>
2024-07-11 00:11:42 +0530

    added navbar to homepage

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
 import Footer from './Footer';
-import Header from './Header';
+import Navbar from './Navbar';
 
 const Layout = () => {
   return (
     <div>
-      <Header />
+      <Navbar />
         <Outlet />
       <Footer />
     </div>
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faInfoCircle, faEnvelope, faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src="/images/logo.jpeg" alt="Logo" />
+      </div>
+      <ul className="nav-links">
+        <li><Link to="#home">Home</Link></li>
+        <li><Link to="#about">About</Link></li>
+        <li><Link to="#rent">Rent</Link></li>
+        <li><Link to="#thrift">Thrift</Link></li>
+        <li><Link to="#wishlist"><FontAwesomeIcon icon={faHeart} /></Link></li>
+        <li><Link to="#cart"><FontAwesomeIcon icon={faShoppingCart} /></Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
