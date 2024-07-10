615d4c14d86a56fe944c46378db0402f8ee9ee9c
krishn113 <ag.krishna0807@gmail.com>
2024-07-10 17:58:05 +0530

    added frontend structure

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink , Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div>
+      <header className='header-top-strip py-3'>
+        <div>
+          text here
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
