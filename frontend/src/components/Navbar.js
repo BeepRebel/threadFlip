93b6b50f011f29829775d5433f0d31b652921d3e
BeepRebel <139386209+BeepRebel@users.noreply.github.com>
2024-07-11 16:36:04 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,13 +8,15 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-        <img src="/images/logo.jpeg" alt="Logo" />
+        <img src="/images/logo.png" alt="Logo"/>
       </div>
       <ul className="nav-links">
         <li><Link to="#home">Home</Link></li>
         <li><Link to="#about">About</Link></li>
-        <li><Link to="/Rent">Rent</Link></li>
-        <li><Link to="/product">Thrift</Link></li>
+        <li><Link to="#rent">Rent</Link></li>
+        <li><Link to="/thrift">Thrift</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
         <li><Link to="#wishlist"><FontAwesomeIcon icon={faHeart} /></Link></li>
         <li><Link to="#cart"><FontAwesomeIcon icon={faShoppingCart} /></Link></li>
       </ul>
