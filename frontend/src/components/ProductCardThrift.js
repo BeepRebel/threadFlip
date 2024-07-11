b9bbad01fc5c7b4981554fb169d24256b0b7c32b
krishn113 <ag.krishna0807@gmail.com>
2024-07-11 12:24:46 +0530

    product card update

--- frontend/src/components/ProductCardThrift.js
+++ frontend/src/components/ProductCardThrift.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import ReactStars from 'react-rating-stars-component';
+import ReactStars from "react-rating-stars-component";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import { faShoppingCart, faEye, faHeart } from '@fortawesome/free-solid-svg-icons';
+
 import './ProductCardThrift.css';
 
-const ProductCardThrift = () => {
+const ProductCardThrift = ({ brand, title, price, image, rating }) => {
   return (
-    <div className='col-3'>
-      <div className='product-card position-relative'>
-        <div className='product-image'>
-          <img src="/images/home/home-banner-1.png" alt="product-image"/>
-          <div className='wishlist-icon'>
-            33
-          </div>
+    <div className='col-md-3'>
+      <div className='card product-card position-relative'>
+        <div className='wishlist-icon position-absolute'>
+        <Link to="#wishlist" className='wishlist-icon'><FontAwesomeIcon icon={faHeart} /></Link>
         </div>
-        <div className='product-details'>
-          <h6 className='brand'>Havels</h6>
-          <h5 className='product-title'>product name</h5>
-          <p className='price'>price here</p>
-        </div>
-        <div className='action-bar position-absolute'>
-            <div className='d-flex flex-column gap-15'>icon</div>
-            <div className='d-flex flex-column gap-15'>icon</div>
-            <div className='d-flex flex-column gap-15'>icon</div>
-            <div className='d-flex flex-column gap-15'>icon</div>
+        <img src={image} className='card-img-top' alt="product-image" />
+        <div className='card-body'>
+          <h6 className='card-subtitle mb-2 text-muted'>{brand}</h6>
+          <h5 className='card-title'>{title}</h5>
+          <p className='card-text'>₹{price}</p>
+          <ReactStars
+            count={5}
+            size={24}
+            edit={false}
+            value={rating}
+            activeColor="#ffd700"
+          />
+          <button className='btn btn-primary mt-3 w-100'>Thrift</button>
         </div>
       </div>
     </div>
@@ -29,3 +33,4 @@ const ProductCardThrift = () => {
 };
 
 export default ProductCardThrift;
+
