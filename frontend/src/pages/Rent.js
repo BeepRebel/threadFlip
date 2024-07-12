1c08d863b5435081817f3524afd6c14a02c161b7
BeepRebel <139386209+BeepRebel@users.noreply.github.com>
2024-07-12 12:06:57 +0530

    Update Rent.js

--- frontend/src/pages/Rent.js
+++ frontend/src/pages/Rent.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CarouselComponent from './CarouselComponent';
 import ButtonsComponent from './ButtonsComponent';
 
 const Rent = () => {
+  useEffect(() => {
+    const sections = document.querySelectorAll('.slide-in-bottom');
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+
+    // Cleanup function to unobserve elements when component unmounts
+    return () => {
+      sections.forEach(section => {
+        observer.unobserve(section);
+      });
+    };
+  }, []);
   return (
     <>
     <div className='store-wrapper home-wrapper-2 py-5 fade-in'>
