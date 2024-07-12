104826f9b722b8dd2087124b686e02bdb580b2b5
BeepRebel <139386209+BeepRebel@users.noreply.github.com>
2024-07-12 11:59:06 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from 'react';
+import { Carousel } from 'react-bootstrap';
+import { FaFacebook, FaInstagram, FaEnvelope } from 'react-icons/fa';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 
 const Home = () => {
@@ -37,8 +40,10 @@ const Home = () => {
       <div className='container-xxl'>
         <div className='row'>
           <div className='col-12 mb-3'>
-            <div className='fade-in' >
-              <img src="/images/home/banner.png" className='banner' alt="main-banner" />
+            <div className='fade-in'>
+            <img src="/images/home/Picture1.png" className='bannern ' alt="main-banner" />
+            
+            
               <div className='comm slide-in-bottom'>
                 <p>Join our fashion community</p>
                 <button className='log-sign'>Login/SignUp</button>
@@ -48,21 +53,74 @@ const Home = () => {
                 <button className='rent'>Thrift</button>
                 <p>Discover Latest Trends</p>
               </div>
-            </div>
+              
+              <img src="/images/home/banner.png" className='banner slide-in-bottom' alt="main-banner" />
+          <div className='car col-12 mb-3'>
+            <Carousel controls={false} indicators={false} interval={2000} className='slide-in-bottom'>
+              <Carousel.Item>
+                <div className='d-flex justify-content-between'>
+                  <div >
+                    <div className='main-banner position-relative p-3'>
+                      <img src="/images/home/home-banner-1.png" className='img-fluid rounded-3 w-100' alt="main-banner" />
+                      <button className='button position-absolute' style={{ bottom: '10px', left: '10px' }}>Buy Now</button>
+                    </div>
+                  </div>
+                  <div >
+                    <div className='main-banner position-relative p-3'>
+                      <img src="/images/home/home-banner-1.png" className='img-fluid rounded-3 w-100' alt="main-banner" />
+                      <button className='button position-absolute' style={{ bottom: '10px', left: '10px' }}>Buy Now</button>
+                    </div>
+                  </div>
+                  <div >
+                    <div className='main-banner position-relative p-3'>
+                      <img src="/images/home/home-banner-1.png" className='img-fluid rounded-3 w-100' alt="main-banner" />
+                      <button className='button position-absolute' style={{ bottom: '10px', left: '10px' }}>Buy Now</button>
+                    </div>
+                  </div>
+                </div>
+              </Carousel.Item>
+              <Carousel.Item>
+                <div className='d-flex justify-content-between'>
+                  <div >
+                    <div className='main-banner position-relative p-3'>
+                      <img src="/images/home/home-banner-1.png" className='img-fluid rounded-3 w-100' alt="main-banner" />
+                      <button className='button position-absolute' style={{ bottom: '10px', left: '10px' }}>Buy Now</button>
+                    </div>
+                  </div>
+                  <div >
+                    <div className='main-banner position-relative p-3'>
+                      <img src="/images/home/home-banner-1.png" className='img-fluid rounded-3 w-100' alt="main-banner" />
+                      <button className='button position-absolute' style={{ bottom: '10px', left: '10px' }}>Buy Now</button>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='main-banner position-relative p-3'>
+                      <img src="/images/home/home-banner-1.png" className='img-fluid rounded-3 w-100' alt="main-banner" />
+                      <button className='button position-absolute' style={{ bottom: '10px', left: '10px' }}>Buy Now</button>
+                    </div>
+                  </div>
+                </div>
+              </Carousel.Item>
+            </Carousel>
           </div>
-          <div className='col-md-6 mb-3 slide-in-bottom'>
-            <div className='main-banner position-relative p-3'>
-              <img src="/images/home/home-banner-1.png" className='img-fluid rounded-3 w-100' alt="main-banner" />
-              <button className='button position-absolute' style={{ bottom: '10px', left: '10px' }}>Buy Now</button>
-            </div>
+          <div className='similar slide-in-bottom'>
+            <button className='button center'>Discover similar items</button>
           </div>
-          <div className='col-md-6 mb-3 slide-in-bottom'>
-            <div className='main-banner position-relative p-3'>
-              <img src="/images/home/home-banner-1.png" className='img-fluid rounded-3 w-100' alt="main-banner" />
-              <button className='button position-absolute' style={{ bottom: '10px', left: '10px' }}>Buy Now</button>
-            </div>
-          </div>
+          <div className='comm slide-in-bottom stay col-12 mb-3'>
+                <p>Stay Connected</p>
+                <div className='social-icons'>
+                  <a href="https://facebook.com" className='icon'><FaFacebook /></a>
+                  <a href="https://instagram.com" className='icon'><FaInstagram /></a>
+                  <a href="mailto:info@example.com" className='icon'><FaEnvelope /></a>
+                </div>
+              </div>
+              
+              <div className='trend slide-in-bottom new'>
+                <img src="/images/home/gown.gif" className='gif'></img>
+                <p>Join Sustainable Fashion Movement</p>
+              </div>
         </div>
+        </div></div>
       </div>
     </section>
   );
